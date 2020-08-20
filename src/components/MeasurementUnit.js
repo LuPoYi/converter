0309ec8ae978ffef6907333c21db9fdf713a5566
Bob <maydaybob2000@gmail.com>
2020-08-20 14:52:01 +0800

    Add kg to pound

--- src/components/MeasurementUnit.js
+++ src/components/MeasurementUnit.js
@@ -2,26 +2,46 @@ import React, { Component } from 'react';
 
 
 class MeasurementUnit extends Component {
+  cmInchRatio = 0.3937
+  kgPoundRatio = 2.2046226218
+
   constructor() {
     super()
     this.state = {
       cmInput: 1,
       inchInput: 0.3937,
+      kgInput: 1,
+      poundInput: 2.2046226218,
+
     }
     this.cmHandleChange = this.cmHandleChange.bind(this)
     this.inchHandleChange = this.inchHandleChange.bind(this)
+    this.kgHandleChange = this.kgHandleChange.bind(this)
+    this.poundHandleChange = this.poundHandleChange.bind(this)
   }
 
   cmHandleChange(event) {
     this.setState({
-      cmInput: event.target.value,
-      inchInput: (event.target.value * 0.3937).toFixed(4)
+      kgInput: event.target.value,
+      poundInput: (event.target.value * this.cmInchRatio).toFixed(4)
     })
   }
   inchHandleChange(event) {
     this.setState({
-      cmInput: (event.target.value / 0.3937).toFixed(4),
-      inchInput: event.target.value
+      kgInput: (event.target.value / this.cmInchRatio).toFixed(4),
+      poundInput: event.target.value
+    })
+  }
+  kgHandleChange(event) {
+    this.setState({
+      kgInput: event.target.value,
+      poundInput: (event.target.value * this.kgPoundRatio).toFixed(4)
+    })
+  }
+  poundHandleChange(event) {
+    this.setState({
+      kgInput: (event.target.value / this.kgPoundRatio).toFixed(4),
+      poundInput: event.target.value
     })
   }
 
@@ -34,6 +54,12 @@ class MeasurementUnit extends Component {
         </div>
         <div className="item">
           inch: <input onChange={this.inchHandleChange} value={this.state.inchInput}></input>
+        </div>
+        <div className="item">
+          kg: <input onChange={this.kgHandleChange} value={this.state.kgInput}></input>
+        </div>
+        <div className="item">
+          pound: <input onChange={this.poundHandleChange} value={this.state.poundInput}></input>
         </div>
       </div >
     )
